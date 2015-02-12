361ba2e49614886ed07bbeea0f84bc64ba6343ce
Xavier Sosnovsky <xso@sosna.ws>
2015-02-12 16:37:04 +0100

    Added an option to suspend/resume all jobs

--- client/templates/jobs/jobs_list.js
+++ client/templates/jobs/jobs_list.js
@@ -1,5 +1,52 @@
 Template.jobsList.helpers({
     jobs: function() {
         return Jobs.find( { isDeleted: false } );
+    },
+
+    isActive: function() {
+        var active = 0;
+        var suspended = 0;
+        this.forEach(function(item) {
+            if (item.isActive) {
+                active++;
+            } else {
+                suspended++;
+            }
+        });
+        return active >= suspended;
+    }
+});
+
+Template.jobsList.events({
+    'click #suspend': function(e) {
+        e.preventDefault();
+        e.stopImmediatePropagation();
+        this.forEach(function(item) {
+            item.isActive = false;
+            Meteor.call('jobUpdate', item, function(error, result) {
+                if (error) {
+                    return alert(error.reason);
+                }
+                if (result.urlExists) {
+                    return alert('There is already a monitoring job for the supplied URL.');
+                }
+            });
+        });
+
+    },
+    'click #resume': function(e) {
+        e.preventDefault();
+        e.stopImmediatePropagation();
+        this.forEach(function(item) {
+            item.isActive = true;
+            Meteor.call('jobUpdate', item, function(error, result) {
+                if (error) {
+                    return alert(error.reason);
+                }
+                if (result.urlExists) {
+                    return alert('There is already a monitoring job for the supplied URL.');
+                }
+            });
+        });
     }
 });
