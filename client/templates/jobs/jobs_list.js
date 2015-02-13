0c58f810aaa78108de85509269cd968c6fb24020
Sébastien Leroy <Leroy.milamber@gmail.com>
2015-02-13 12:31:31 +0000

    Preparation of the master detail jobs monitoring template.

--- client/templates/jobs/jobs_list.js
+++ client/templates/jobs/jobs_list.js
@@ -3,50 +3,64 @@ Template.jobsList.rendered = function() {
 };
 
 Template.jobsList.helpers({
-    isActive: function() {
-        var active = 0;
-        var suspended = 0;
-        this.forEach(function(item) {
-            if (item.isActive) {
-                active++;
-            } else {
-                suspended++;
-            }
-        });
-        return active >= suspended;
+  isActive: function() {
+    var active = 0;
+    var suspended = 0;
+    this.forEach(function(item) {
+      if (item.isActive) {
+        active++;
+      } else {
+        suspended++;
+      }
+    });
+    return active >= suspended;
+  },
+  settings: function () {
+    return {
+      rowsPerPage: 15, 
+      showNavigation: "auto",
+      fields: [
+        {key: '_id', label: 'Id'},
+        {key: 'name', label: 'Name'},
+        { key: "actions", label: "Actions", fn:  function (value) {
+          return '{{> monitoringActionButtons}}';
+        }
+        }
+      ]
     }
+  }
 });
 
 Template.jobsList.events({
-    'click #suspend': function(e) {
-        e.preventDefault();
-        e.stopImmediatePropagation();
-        this.forEach(function(item) {
-            item.isActive = false;
-            Meteor.call('jobUpdate', item, function(error, result) {
-                if (error) {
-                    return alert(error.reason);
-                }
-                if (result.urlExists) {
-                    return alert('There is already a monitoring job for the supplied URL.');
-                }
-            });
-        });
+  'click #suspend': function(e) {
+    e.preventDefault();
+    e.stopImmediatePropagation();
+    this.forEach(function(item) {
+      item.isActive = false;
+      Meteor.call('jobUpdate', item, function(error, result) {
+        if (error) {
+          return alert(error.reason);
+        }
+        if (result.urlExists) {
+          return alert('There is already a monitoring job for the supplied URL.');
+        }
+      });
+    });
 
-    },
-    'click #resume': function(e) {
-        e.preventDefault();
-        e.stopImmediatePropagation();
-        this.forEach(function(item) {
-            item.isActive = true;
-            Meteor.call('jobUpdate', item, function(error, result) {
-                if (error) {
-                    return alert(error.reason);
-                }
-                if (result.urlExists) {
-                    return alert('There is already a monitoring job for the supplied URL.');
-                }
-            });
-        });
-    }
+  },
+  'click #resume': function(e) {
+    e.preventDefault();
+    e.stopImmediatePropagation();
+    this.forEach(function(item) {
+      item.isActive = true;
+      Meteor.call('jobUpdate', item, function(error, result) {
+        if (error) {
+          return alert(error.reason);
+        }
+        if (result.urlExists) {
+          return alert('There is already a monitoring job for the supplied URL.');
+        }
+      });
+    });
+  }
 });
