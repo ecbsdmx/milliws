449263f22081dc1f22d844fe934e068579aebf72
Xavier Sosnovsky <xso@sosna.ws>
2015-02-13 18:28:47 +0100

    Fixed issue with chevron

--- client/templates/jobs/job_item.js
+++ client/templates/jobs/job_item.js
@@ -1,9 +1,10 @@
-Template.jobItem.rendered = function() {
-  $('.detail_'+ this._id).on('shown.bs.collapse', function () {
-    $(".chevron").removeClass("fa-chevron-down").addClass("fa-chevron-up");
-  });
-
-  $('.detail_'+ this._id).on('hidden.bs.collapse', function () {
-    $(".chevron").removeClass("fa-chevron-up").addClass("fa-chevron-down");
-  });
-};
+Template.jobItem.events({
+  'click #chevron': function(e) {
+    var id = "#chevron_" + this._id;
+    if($(id).hasClass( "fa-chevron-down" )) {
+      $(id).removeClass("fa-chevron-down").addClass("fa-chevron-up");
+    } else {
+      $(id).removeClass("fa-chevron-up").addClass("fa-chevron-down");
+    }
+  }
+});
