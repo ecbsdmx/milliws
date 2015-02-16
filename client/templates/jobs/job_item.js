c3955a520d37a8b5a4c8cca6178fdf98f9e677ad
Sébastien Leroy <Leroy.milamber@gmail.com>
2015-02-16 13:19:00 +0000

    Chevron toggle on job row click. Edit & delete button re-created

--- client/templates/jobs/job_item.js
+++ client/templates/jobs/job_item.js
@@ -1,10 +1,38 @@
-Template.jobItem.events({
-  'click #chevron': function(e) {
-    var id = "#chevron_" + this._id;
-    if($(id).hasClass( "fa-chevron-down" )) {
-      $(id).removeClass("fa-chevron-down").addClass("fa-chevron-up");
-    } else {
-      $(id).removeClass("fa-chevron-up").addClass("fa-chevron-down");
-    }
+function toggleChevron(id) {
+  var chevronId = "#chevron_" + id;
+  var detailId = "#detail_" + id;
+
+  $(detailId).toggleClass("in");
+
+  if($(chevronId).hasClass( "fa-chevron-down" )) {
+    $(chevronId).removeClass("fa-chevron-down").addClass("fa-chevron-up");
+  } else {
+    $(chevronId).removeClass("fa-chevron-up").addClass("fa-chevron-down");
   }
+}
+
+
+Template.jobItem.events({
+  'click .jobItemRow': function(e) {
+    e.preventDefault();
+    toggleChevron(this._id);
+  },
+  'click .jobItemRow .edit': function(e) {
+    e.preventDefault();
+    e.stopImmediatePropagation();
+    Router.go('jobEdit', {_id: this._id});
+  },
+  'click .jobItemRow .delete': function(e) {
+    e.preventDefault();
+    e.stopImmediatePropagation();
+    if (confirm("Are you sure you want to delete this monitoring job?")) {
+      this.isDeleted = true;
+      Meteor.call('jobUpdate', this, function(error, result) {
+        if (error) {
+          return alert(error.reason);
+        }
+        Router.go('jobsList');
+      });
+    }
+  }  
 });
