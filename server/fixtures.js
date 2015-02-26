15592171432c4af0facc3cdf3167b6d9fcc713d5
Xavier Sosnovsky <xso@sosna.ws>
2015-02-26 08:02:55 +0100

    Insertion of fixtures now use proper server method

--- server/fixtures.js
+++ server/fixtures.js
@@ -21,29 +21,25 @@ if (Meteor.users.find().count() === 0) {
 
 // Add default jobs (only for testing)
 if (Jobs.find().count() === 0) {
- Jobs.insert({
-    _id: "dexr",
-    name: "Daily exchange rates",
-    url: "http://a-sdw-wsrest.ecb.europa.eu/service/data/EXR/D.NOK+CAD+RUB.EUR.SP00.A",
-    ert: 1000,
-    freq: 1,
-    creationDate: new Date(),
-    isDeleted: false,
-    isActive: true,
-    deltas: true,
-    isCompressed: true,
-    isIMS: false,
-    format: "sdmx-json-1.0.0"
-  });
-  Jobs.insert({
+
+  Meteor.call('jobInsert', {
+     _id: "dexr",
+     name: "Daily exchange rates",
+     url: "http://a-sdw-wsrest.ecb.europa.eu/service/data/EXR/D.NOK+CAD+RUB.EUR.SP00.A",
+     ert: 1000,
+     freq: 1,
+     deltas: true,
+     isCompressed: true,
+     isIMS: false,
+     format: "sdmx-json-1.0.0"
+   });
+
+   Meteor.call('jobInsert', {
     _id: "m1m3",
     name: "Monetary aggregates",
     url: "http://a-sdw-wsrest.ecb.europa.eu/service/data/BSI/M.U2.Y.V.M10+M30.X.I.U2.2300.Z01.A+E",
     ert: 1000,
-    creationDate: new Date(),
     freq: 1,
-    isDeleted: false,
-    isActive: true,
     deltas: false,
     isCompressed: true,
     isIMS: false,
