cb9343e02f937a884b7aa7c48eef5024d1d53898
Xavier Sosnovsky <xso@sosna.ws>
2015-03-03 13:33:20 +0100

    Added explanation for status code

--- TODO.md
+++ TODO.md
@@ -12,7 +12,7 @@
 - [ ] Store the content-length for data size comparison (compressed or not, delta, etc.) ???result.content.length compressed via http-more & other prop???
 - [ ] Job column: When clicking on the job id, display information about the job.
 - [ ] Executed on column: Find the best format for displaying the timestamp.
-- [ ] HTTP status code column: When clicking on the status code, display the meaning of the code.
+- [x] HTTP status code column: When clicking on the status code, display the meaning of the code.
 - [x] HTTP status code column: Should be in red if not 200, 304 or 404.
 - [ ] Expected response time and response time should be in the same column and displayed as a gauge. The problematic ones should stand out.
 - [ ] There should be an option to only show the problematic entries.
--- client/templates/events/events_status.js
+++ client/templates/events/events_status.js
@@ -1,5 +1,9 @@
+Template.statusCodeCell.rendered = function() {
+  $('[data-toggle="tooltip"]').tooltip();
+};
+
 Template.statusCodeCell.helpers({
-  class: function(status) {
+  statusClass: function(status) {
     switch (status) {
       case 404:
         return "text-warning";
@@ -8,6 +12,35 @@ Template.statusCodeCell.helpers({
         return "text-primary";
       default:
         return "text-danger";
+    }
+  },
+
+  statusText: function(status) {
+    switch (status) {
+      case 200:
+        return "Life is good.";
+      case 304:
+        return "No change since the timestamp supplied in the If-Modified-Since header.";
+      case 400:
+        return "Syntax error. Your query checking you must.";
+      case 401:
+        return "Only available on a need to know basis. Login needed.";
+      case 403:
+        return "Semantic error. The syntax of the query is OK but it makes no sense anyway. Tolerated only on April Fools.";
+      case 404:
+        return "No results. Mostly harmless, especially if you used updatedAfter.";
+      case 406:
+        return "Not a supported format. No worries, SDMX is all you need.";
+      case 413:
+        return "Results too large. You know greed is a sin, right?";
+      case 500:
+        return "Server error. Someone somewhere is having a bad day.";
+      case 501:
+        return "Not implemented. Feature coming soon in a web service near you.";
+      case 503:
+        return "Service unavailable. Try again later.";
+      default:
+        return "Unknown error code.";
     }
   }
 });
