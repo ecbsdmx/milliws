5d46f47a86c5810323854cee86f694354fdc0540
Xavier Sosnovsky <xso@sosna.ws>
2015-03-24 14:42:02 +0100

    Prepared submission of events filters from clients

--- client/views/events/column_filter.js
+++ client/views/events/column_filter.js
@@ -2,16 +2,16 @@ Template.columnFilter.rendered = function(){
   $('[data-toggle="popover"]').popover();
 }
 
-Template.columnFilter.helpers({
-  getHelpInfo: function(help){
-    console.log("help text = " + help);
-    return help;
-  }
-});
-
 Template.columnFilter.events({
   'click .filtersMenu a': function (e){
     var symbol = $(e.currentTarget).find(".symbol").first().text();
     $(e.currentTarget).closest('div').find("button").first().text(symbol);
+  },
+  'keypress input': function(e) {
+    if (e.which === 13) {
+      e.preventDefault();
+      e.stopPropagation();
+      $(e.currentTarget).closest("form").submit();
+    }
   }
 });
