40fc33b26f0fc80363a1a45591a2f9ddd219ce89
Tzaphkiel <Leroy.milamber@gmail.com>
2015-03-25 16:31:32 +0100

    Implemented tag click focus filter field content.

--- client/views/events/components/events_active_filters.js
+++ client/views/events/components/events_active_filters.js
@@ -16,5 +16,16 @@ Template.eventsActiveFilters.events({
     //FIXME code remove filter btn
     var fieldId = $(e.currentTarget).attr("id").substr("removeField_".length);
     console.log("to be coded! field: " + fieldId);
+  },
+  'click .label': function(e) {
+    var fieldId = $(e.currentTarget).find("span").last().attr("id").substr("removeField_".length);
+    console.log("Clicked on tag: " + fieldId);
+
+    //FIXME call parent function that does this (code once + re-use)
+    $("#filtersRow").removeClass('hide');
+    $("#runFiltersBtn").removeClass('hide');
+
+    $("#"+fieldId).focus().select();
+
   }
 });
