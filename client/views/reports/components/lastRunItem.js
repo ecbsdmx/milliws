6b524fdf8cf6794ba5dd477d46975449895010a8
Xavier Sosnovsky <xso@sosna.ws>
2015-04-15 13:02:24 +0200

    Added tooltip with job name in the reports section

--- client/views/reports/components/lastRunItem.js
+++ client/views/reports/components/lastRunItem.js
@@ -1,5 +1,9 @@
 Template.lastRunItem.helpers({
   isProblematic: function(){
     return this.lastProb ? "text-danger" : "text-success";
+  },
+
+  jobName: function(id){
+    return Jobs.findOne({_id: id}).name;
   }
 });
