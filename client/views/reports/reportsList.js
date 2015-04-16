41b497d5ca2b143e221d696b45069e76266c9973
Xavier Sosnovsky <xso@sosna.ws>
2015-04-16 15:40:39 +0200

    Added toggle job buttons in the report section

--- client/views/reports/reportsList.js
+++ client/views/reports/reportsList.js
@@ -1,10 +1,17 @@
-Template.reportsList.rendered = function() {
-};
-
-
 Template.reportsList.helpers({
-});
+  eventStats: function() {
+    return EventStats.find({}, {sort: {_id: 1}});
+  },
 
-
-Template.reportsList.events({
+  filteredEventStats: function() {
+    if (!Session.get("SelectedEventsStats")) {
+      var events = EventStats.find({}, {sort: {_id: 1}, fields: {_id: 1}});
+      var selectedEvents = [];
+      events.forEach(function(element){
+        selectedEvents.push(element._id);
+      });
+      Session.set("SelectedEventsStats", selectedEvents);
+    };
+    return EventStats.find({_id: {$in: Session.get("SelectedEventsStats")}}, {sort: {_id: 1}});
+  }
 });
