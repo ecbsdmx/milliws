621cc68e932ab9170019a861e442e45db5db84cd
Tzaphkiel <Leroy.milamber@gmail.com>
2015-04-20 16:29:28 +0200

    Integrated Calendar heat map d3 component.

--- client/views/reports/components/month-monday-cal-heatmap.js
+++ client/views/reports/components/month-monday-cal-heatmap.js
@@ -3,375 +3,263 @@ Template.monthMondayCalHeatmap.onCreated(function() {
 });
 
 Template.monthMondayCalHeatmap.rendered = function() {
-  var dat = 
-    { 'dexr-gen': 
-       [ { date: '2015-04-14',
-           count: 468,
-           minRT: 8,
-           maxRT: 30332,
-           avgRT: 286.6837606837607 },
-         { date: '2015-04-13',
-           count: 433,
-           minRT: 17,
-           maxRT: 10480,
-           avgRT: 407.1986143187067 },
-         { date: '2015-04-02',
-           count: 69,
-           minRT: 52,
-           maxRT: 7862,
-           avgRT: 314.0289855072464 },
-         { date: '2015-03-26',
-           count: 115,
-           minRT: 57,
-           maxRT: 6134,
-           avgRT: 395.38260869565215 },
-         { date: '2015-03-25',
-           count: 9,
-           minRT: 82,
-           maxRT: 1031,
-           avgRT: 319.55555555555554 },
-         { date: '2015-03-20',
-           count: 258,
-           minRT: 46,
-           maxRT: 4498,
-           avgRT: 209.60852713178295 },
-         { date: '2015-03-19',
-           count: 206,
-           minRT: 46,
-           maxRT: 3710,
-           avgRT: 168.89320388349515 } ],
-      'dexr-json': 
-       [ { date: '2015-04-13',
-           count: 433,
-           minRT: 45,
-           maxRT: 32793,
-           avgRT: 502.48729792147805 },
-         { date: '2015-04-02',
-           count: 69,
-           minRT: 42,
-           maxRT: 7831,
-           avgRT: 322.4347826086956 },
-         { date: '2015-03-25',
-           count: 9,
-           minRT: 61,
-           maxRT: 766,
-           avgRT: 211.11111111111111 },
-         { date: '2015-03-20',
-           count: 258,
-           minRT: 47,
-           maxRT: 4491,
-           avgRT: 235.15116279069767 },
-         { date: '2015-03-26',
-           count: 115,
-           minRT: 49,
-           maxRT: 5650,
-           avgRT: 358.55652173913046 },
-         { date: '2015-04-14',
-           count: 469,
-           minRT: 46,
-           maxRT: 30509,
-           avgRT: 291.99360341151385 },
-         { date: '2015-03-19',
-           count: 206,
-           minRT: 48,
-           maxRT: 3700,
-           avgRT: 194.38834951456312 } ],
-      'dexr-comp': 
-       [ { date: '2015-04-14',
-           count: 471,
-           minRT: 44,
-           maxRT: 12450,
-           avgRT: 250.5711252653928 },
-         { date: '2015-03-26',
-           count: 115,
-           minRT: 46,
-           maxRT: 6112,
-           avgRT: 329.3652173913043 },
-         { date: '2015-04-13',
-           count: 433,
-           minRT: 42,
-           maxRT: 10464,
-           avgRT: 431.9237875288684 },
-         { date: '2015-03-25',
-           count: 9,
-           minRT: 62,
-           maxRT: 763,
-           avgRT: 304.55555555555554 },
-         { date: '2015-03-20',
-           count: 258,
-           minRT: 41,
-           maxRT: 4475,
-           avgRT: 240.40697674418604 },
-         { date: '2015-03-19',
-           count: 206,
-           minRT: 40,
-           maxRT: 3690,
-           avgRT: 175.07766990291262 },
-         { date: '2015-04-02',
-           count: 69,
-           minRT: 42,
-           maxRT: 7847,
-           avgRT: 322.28985507246375 } ],
-      m1m3: 
-       [ { date: '2015-04-14',
-           count: 470,
-           minRT: 43,
-           maxRT: 30269,
-           avgRT: 282.99787234042554 },
-         { date: '2015-04-02',
-           count: 69,
-           minRT: 43,
-           maxRT: 3587,
-           avgRT: 180.30434782608697 },
-         { date: '2015-03-26',
-           count: 115,
-           minRT: 50,
-           maxRT: 2094,
-           avgRT: 302.8782608695652 },
-         { date: '2015-03-20',
-           count: 258,
-           minRT: 44,
-           maxRT: 4489,
-           avgRT: 223.60077519379846 },
-         { date: '2015-04-13',
-           count: 433,
-           minRT: 47,
-           maxRT: 3203,
-           avgRT: 368.3764434180139 },
-         { date: '2015-03-19',
-           count: 206,
-           minRT: 44,
-           maxRT: 3833,
-           avgRT: 161.60194174757282 },
-         { date: '2015-03-25',
-           count: 9,
-           minRT: 63,
-           maxRT: 776,
-           avgRT: 175.55555555555554 } ],
-      'icp-gz': 
-       [ { date: '2015-04-14',
-           count: 472,
-           minRT: 35,
-           maxRT: 3357,
-           avgRT: 218.4343220338983 },
-         { date: '2015-04-02',
-           count: 69,
-           minRT: 39,
-           maxRT: 3595,
-           avgRT: 191.7391304347826 },
-         { date: '2015-04-13',
-           count: 434,
-           minRT: 38,
-           maxRT: 3221,
-           avgRT: 349.51612903225805 },
-         { date: '2015-03-25',
-           count: 9,
-           minRT: 57,
-           maxRT: 176,
-           avgRT: 91.22222222222223 },
-         { date: '2015-03-20',
-           count: 265,
-           minRT: 42,
-           maxRT: 4487,
-           avgRT: 239.12830188679246 },
-         { date: '2015-03-26',
-           count: 115,
-           minRT: 49,
-           maxRT: 3177,
-           avgRT: 301.9913043478261 },
-         { date: '2015-03-19',
-           count: 206,
-           minRT: 39,
-           maxRT: 3692,
-           avgRT: 180.9757281553398 } ],
-      'icp-fat': 
-       [ { date: '2015-04-02',
-           count: 69,
-           minRT: 34,
-           maxRT: 3622,
-           avgRT: 194.95652173913044 },
-         { date: '2015-03-26',
-           count: 115,
-           minRT: 50,
-           maxRT: 3339,
-           avgRT: 359.7130434782609 },
-         { date: '2015-03-25',
-           count: 9,
-           minRT: 58,
-           maxRT: 176,
-           avgRT: 112.44444444444444 },
-         { date: '2015-04-13',
-           count: 433,
-           minRT: 38,
-           maxRT: 3644,
-           avgRT: 420.4688221709007 },
-         { date: '2015-04-14',
-           count: 474,
-           minRT: 37,
-           maxRT: 3040,
-           avgRT: 211.3438818565401 },
-         { date: '2015-03-19',
-           count: 206,
-           minRT: 46,
-           maxRT: 3790,
-           avgRT: 208.24757281553397 },
-         { date: '2015-03-20',
-           count: 264,
-           minRT: 46,
-           maxRT: 4519,
-           avgRT: 242.8181818181818 } ],
-      mna: 
-       [ { date: '2015-03-19',
-           count: 1,
-           minRT: 222,
-           maxRT: 222,
-           avgRT: 222 } ]
-    };
+var input = {
+    _id: "dexr-gen",
+    max: 30332,
+    avg: 286.6837606837607,
+    count: 1558,
+    min: 8,
+    ert: 1000,
+    last: new Date("2015-04-14"),
+    first: new Date("2015-03-19"),
+    lastProb: false,
+    dayAgg: [
+      { date: '2015-04-14',
+       count: 468,
+       minRT: 8,
+       maxRT: 30332,
+       avgRT: 286.6837606837607 },
+      { date: '2015-02-13',
+       count: 433,
+       minRT: 17,
+       maxRT: 10480,
+       avgRT: 407.1986143187067 },
+      { date: '2015-04-02',
+       count: 69,
+       minRT: 52,
+       maxRT: 7862,
+       avgRT: 314.0289855072464 },
+      { date: '2015-03-26',
+       count: 115,
+       minRT: 57,
+       maxRT: 6134,
+       avgRT: 395.38260869565215 },
+      { date: '2015-03-25',
+       count: 9,
+       minRT: 82,
+       maxRT: 1031,
+       avgRT: 319.55555555555554 },
+      { date: '2015-03-20',
+       count: 258,
+       minRT: 46,
+       maxRT: 4498,
+       avgRT: 209.60852713178295 },
+      { date: '2015-03-19',
+       count: 206,
+       minRT: 46,
+       maxRT: 3710,
+       avgRT: 168.89320388349515 }
+    ]
+  };
+
+//-- formats
+var day     = function(d) { return (d.getDay() + 6) % 7;};
+var week    = d3.time.format("%W");
+var month   = d3.time.format("%b");
+var year    = d3.time.format("%Y");
+var year2   = d3.time.format("%y");
+var date    = d3.time.format("%Y-%m-%d");
+var theDay  = d3.time.format("%A");
+
+//-- options
+var showMonthGroups   = true;
+var showMonthContour  = true;
+var showAllWeekDays   = false;
+
+//-- constants
+var colorScaleSize  = 10;
+var finalWidth      = 1147;//960,
+    finalHeight     = 147;//147;//orig: 105
+var margin          = {top:15.5, right:5.5, bottom:5.5, left:40.5};
+//-- calculated variables
+var width       = finalWidth - margin.left - margin.right;
+var height      = finalHeight - margin.top - margin.bottom;
+var size        = height/7;
+
+//-- data manipulation
+// Get the maximum data date and go back one day,
+// to the beginning of that month.
+var maxDate             = new Date(d3.max(input.dayAgg, function(d) {return d.date;}));
+var minDate             = new Date(maxDate.getFullYear()-1, maxDate.getMonth(), 1); //TODO chk +1day ?
+var minFirstDayOfMonth  = new Date(minDate.getFullYear(), minDate.getMonth(), 1);
+var minWeek             = parseInt(week(minFirstDayOfMonth));
+var minYear             = parseInt(year(minDate));
+var maxYear             = parseInt(year(maxDate));
+//var minDate     = new Date(d3.min(input.dayAgg, function(d) {return d.date;}));
+var numYears    = maxYear - minYear + 1;
+
+var color       = d3.scale.quantize()
+  .domain([0,input.ert])
+  .range(d3.range(colorScaleSize));
+
+var aggByDates  = d3.nest()
+  .key(function(d) {
+    return d.date;
+  })
+  .sortKeys(d3.ascending)
+  .rollup(function(e){return {count: e[0].count, minRT: e[0].minRT, maxRT: e[0].maxRT, avgRT: e[0].avgRT};})
+  .map(input.dayAgg);
+
+//FIXME find a way to have directly in dateRangeDays the formatted date(x) date !
+  //XXX not mindate directly, otherwise start date not includesm in date range...
+var dateRangeDays           = d3.time.days(new Date(minDate.getFullYear(), minDate.getMonth(), minDate.getDate()), maxDate);
+var dateRangeMonths         = d3.time.months(new Date(minDate.getFullYear(), minDate.getMonth(), 1), maxDate);
+
+/* DEBUG */
+console.dir(input);
+console.dir(aggByDates);
+console.log("Min      :" + date(minDate));
+console.log("Max      :" + date(maxDate));
+//console.log("Num years: " + numYears);
+//console.log("Nesting per date for current input:");
+//console.dir(dateRangeDays);
+/* DEBUG */
 
 
+var svg = d3.select(".calHeatMap").selectAll("svg")
+  .data([input])
+  .enter()
+    .append("svg")
+      .attr("class", "jobCalHeatMap greenOrangeRedGrad")
+      .attr("shape-rendering", "crisp-edges")
+      .attr("stroke-linecap","round")
+      .attr("stroke-linejoin","round")
+      .attr("width", finalWidth)
+      .attr("height", finalHeight)
+      .attr("title", function(d) {return d._id;})
+    .append("g")
+      .attr("class", "")
+      .attr("transform", "translate("+margin.left+", " + margin.top + ")")
+;
+//-- job name label
+var calJobLabel =
+svg
+  .append("text")
+  .attr("class", "jobName")
+  .attr("transform", "translate(-" + (margin.left/9*7) + "," + (height/2) + ")rotate(-90)")
+  .attr("text-anchor", "middle")
+  .text(function(d) { return d._id; })
+;
+//-- job year label
+var calYearLabel =
+svg
+  .append("text")
+  .attr("class", "yearLabel")
+  .attr("transform", "translate(-" + (margin.left/9*4) + "," + (height/2) + ")rotate(-90)")
+  .attr("text-anchor", "middle")
+  .text(function(d) { return year(minDate) + "-" + year(maxDate); })
+;
 
-  var margin = {top: 5.5, right: 0, bottom: 15.5, left: 25},
-  width = 960 - margin.left - margin.right,
-  height = 105 - margin.top - margin.bottom,
-  size = height / 7;
-
-  //-- formatters
-  var day = function(d) { return (d.getDay() + 6) % 7; }, // monday = 0
-  month = function(d) { return d.getMonth(); },
-  week = d3.time.format("%W"), // monday-based week number
-  date = d3.time.format("%Y-%m-%d"),
-  percent = d3.format("+.1%");
-
-  //console.log("years: [" + minYear + ", " + maxYear + "]");
-  
-  //-- rows for each year 
-  var jobCalHeatMap = d3.select(".calHeatMap").selectAll("svg")
-    .data(d3.entries(dat))
-    .enter()
-      .append("svg")
-      .attr("class", "jobCalHeatMap")
-      .attr("title", function(d) { return d.key})
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-  //function(d) { return d.key}
-  //-- row job label
-  jobCalHeatMap.append("text")
-    .attr("transform", "translate(-18," + size * 3.5 + ")rotate(-90)")
+//-- week days label
+var weekDays = ["M", "T", "W", "T", "F", "S", "S"];
+var calWeekDayLabels =
+svg
+  .append("g")
+  .attr("class", "jobWeekDays")
+  .selectAll("text")
+  .data(function(d) {return weekDays;})
+  .enter()
+    .append("text")
+    .attr("x", -margin.left/9*2)
+    .attr("y", function(d, i) { return i*size+margin.top-size/2;})
+    .attr("class", "dayName")
     .attr("text-anchor", "middle")
-    .text(function(d) { return d.key; });
-  
-  
-  var svg = jobCalHeatMap.selectAll("g")
-    .data(function(d) {
-      var jobId = d.key;
-      var jobStat = d.value;
-      var minDate = d3.min(jobStat, function(d) {return new Date(d.date)}); 
-      var maxDate = d3.max(jobStat, function(d) {return new Date(d.date)}); 
-      return d3.range(maxDate.getFullYear(), minDate.getFullYear()-1, -1)
-    })
-    .enter()
-      .append("g")
-      .attr("class", "greenOrangeRedGrad");
+    .text(function(d, i) {return showAllWeekDays?d:(i%2 === 0?d:"");})
+;
+//weeks for each years
 
-  //-- row year label
-  svg.append("text")
-    .attr("transform", "translate(-6," + size * 3.5 + ")rotate(-90)")
-    .attr("text-anchor", "middle")
-    .text(function(d) { return d; });
-    
-    
-
-  //-- day squares (only range where data is available)
-  var rect = svg.selectAll(".day")
-    .data(function(d) {
-      var minD = new Date(d, 0, 1);//minDate;//
-      var maxD = new Date(d + 1, 0, 1);//maxDate;//
-      var today = new Date();
-      return d3.time.days(minD, maxD > today?today:maxD); })
+var calDays =
+svg
+  .append("g")
+  .attr("class", "jobDays")
+  .selectAll("rect")
+    .data(dateRangeDays)
     .enter()
       .append("rect")
       .attr("class", "day")
+      .attr("theDate", function(d) { return date(d);})
       .attr("width", size)
       .attr("height", size)
-      .attr("x", function(d) { return week(d) * size; })
-      .attr("y", function(d) { return day(d) * size; })
-      .each(function(d, i) {this.__data__ = date(d);})
-      //.map(date)
-      ;
+      .attr("x", function(d) {
+        var yearDecal = year(d) - year(minDate);
+        return (week(d)-minWeek + 52*yearDecal)  * size;
+      })
+      .attr("y", function(d) {return day(d) * size;})
+      .append("title")
+        .text(function(d) {return date(d) + " : " + theDay(d);})
+;
 
-  //-- tooltip of day box
-  rect.append("title")
-    .text(function(d) { return d; });
-
-  //-- month contour path
-  svg.selectAll(".month")
-    .data(function(d) { 
-      var minD = new Date(d, 0, 1);
-      var maxD = new Date(d + 1, 0, 1);
-      var today = new Date();
-      return d3.time.months(minD, maxD > today?today:maxD); })
+if (showMonthGroups) {
+  //months for each years
+  var calMonthLabels =
+    svg
+    .append("g").attr("class", "jobMonths")
+    .selectAll(".month")
+    .data(dateRangeMonths)
     .enter()
-      .append("path")
-      .attr("class", "month")
-      .attr("d", monthPath);
+    .append("text")
+    .attr("x", function(d, i) {
+      var yearDecal = year(d) - year(minDate);
+      return  (week(d)-minWeek + 52*yearDecal)  * size;
+    })
+    .attr("y", -5)
+    .attr("class", "monthName")
+    .text(function(d) {
+      var m = month(d);
+      return m === 'Jan'?month(d) + "'" + year2(d):m;
+    })
+    ;
 
-  // svg.selectAll(".month")
-  //   .append("text")
-  //   .attr("transform", "translate("+ size +"," + (size*7 - margin.bottom/2) + ")") //FIXME x=size*num weeks in month
-  //   .attr("text-anchor", "middle")
-  //   .text("Jan");
-
-  updateData(dat);
-
-  function monthPath(t0) {
-    var t1 = new Date(t0.getFullYear(), t0.getMonth() + 1, 0),
-    d0 = +day(t0), w0 = +week(t0),
-    d1 = +day(t1), w1 = +week(t1);
-    return "M" + (w0 + 1) * size + "," + d0 * size
-    + "H" + w0 * size + "V" + 7 * size
-    + "H" + w1 * size + "V" + (d1 + 1) * size
-    + "H" + (w1 + 1) * size + "V" + 0
-    + "H" + (w0 + 1) * size + "Z";
+  if (showMonthContour) {
+    //-- months contour path
+    var calMonthContours =
+    svg.select(".jobMonths")
+    .selectAll(".monthPath")
+    .data(dateRangeMonths)
+    .enter()
+    .append("path")
+    .attr("class", "monthPath")
+    .attr("d", monthPath)
+    ;
   }
-};
+}
 
-var updateData = function(dat) {
-  var svg = d3.select(".calHeatMap").selectAll("svg");
 
-  var dd = d3.nest()
-    .key(function(d){return d.date})
-    .sortKeys(d3.ascending)
-    .rollup(function(d) { 
-      return {count: d[0].count,
-          minRT: d[0].minRT,
-          maxRT: d[0].maxRT,
-          avgRT: d[0].avgRT.toFixed(2)
-        };
+updateData();
+
+console.log("input.ert: " + input.ert);
+
+//===================
+//-- Functions
+//===================
+function updateData() {
+  svg.selectAll("g.jobDays .day")
+    .filter(function(d) {
+      return date(d) in aggByDates;
     })
-    .map(dat['dexr-json']);
+  .attr("class", function(d) {
+    var obj = aggByDates[date(d)];
+    //console.log("color: " + color(obj.avgRT));
+    return "day c" + color(obj.avgRT)  ;
+  })
+  ;
+}
 
 
-  var datMin = d3.min(dat['dexr-json'], function(d) {return d.avgRT});
-  var datMax = d3.max(dat['dexr-json'], function(d) {return d.avgRT});
-  console.log("min: " + datMin + ", max: " + datMax);
-
-  //-- scales (color)
-  var color = d3.scale.quantize()
-    .domain([datMin, datMax])
-    .range(d3.range(10));
-
-  svg.selectAll(".day")
-    .filter(function(d) { return d in dd; })
-    .attr("class", function(d) { 
-      return "day c" + color(dd[d].avgRT); 
-    })
-    .select("title")
-    .text(function(d){ 
-      return dd[d].date + ": " + dd[d].avgRT + " (#:" + dd[d].count + ")";
-    });
+function monthPath(t0) {
+  var yearDecal = year(t0) - year(minDate);
+  var t1 = new Date(t0.getFullYear(), t0.getMonth() + 1, 0);
+  var w0 = +(week(t0)-minWeek + 52*yearDecal);
+  var w1 = +(week(t1)-minWeek + 52*yearDecal);
+  var
+      d0 = +day(t0),
+      d1 = +day(t1);
+  return "M" + (w0 + 1) * size + "," + d0 * size
+  + "H" + w0 * size + "V" + 7 * size
+  + "H" + w1 * size + "V" + (d1 + 1) * size
+  + "H" + (w1 + 1) * size + "V" + 0
+  + "H" + (w0 + 1) * size + "Z";
+}
 }
 
 
