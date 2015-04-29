ed59d566db531d973446a09bec3a8427a77c9caa
Xavier Sosnovsky <xso@sosna.ws>
2015-04-29 16:32:00 +0200

    Added box plot for response times

---
+++ client/views/reports/components/responseTimeItem.js
@@ -0,0 +1,40 @@
+Template.responseTimeItem.helpers({
+  graphData: function() {
+    var events = EventStats.find({}, {sort: {_id: 1}});
+    var max = 0;
+    events.forEach(function(element) {
+      if (element.value.whiskerStop > max) {
+        max = element.value.whiskerStop;
+      }
+    });
+    var divider = Math.pow(10, max.toString().length - 1);
+    var maxRange = Math.round((max * 1.1) / divider) * divider;
+    var numTicks = 5;
+    var ticks = [];
+    var step = Math.round(maxRange / numTicks);
+
+    for (var i = 0; i <= numTicks; i++) {
+      ticks[i] = {
+        tickValue:    i * step,
+        tickPercent:  (100 / numTicks) * i + "%"
+      };
+    }
+
+    var boxWidth = (((this.value.quartile3 - this.value.quartile1) / maxRange) * 100) + "%";
+    var boxStart = ((this.value.quartile1 / maxRange) * 100) + "%";
+
+    var whiskerWidth = (((this.value.whiskerStop - this.value.min) / maxRange) * 100) + "%";
+    var whiskerStart = ((this.value.min / maxRange) * 100) + "%";
+
+    var medianStart = ((this.value.quartile2 / maxRange) * 100) + "%";
+
+    return {
+      ticks: ticks,
+      boxWidth: boxWidth,
+      boxStart: boxStart,
+      whiskerWidth: whiskerWidth,
+      whiskerStart: whiskerStart,
+      medianStart: medianStart
+    }
+  }
+});
